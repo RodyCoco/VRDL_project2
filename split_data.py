9f2c62efd686c3a8c4193aa01d6a1d6de3099703
RodyCoco <55498247+RodyCoco@users.noreply.github.com>
2021-11-25 20:51:02 +0800

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -21,8 +21,8 @@ shutil.copytree(
     dirs_exist_ok=False)
 
 image_size_list = get_all_image_size("train/train", 33402)
-digit_data_answer_dict =
-mat73.loadmat('./train/train/digitStruct.mat', use_attrdict=True)
+digit_data_answer_dict = \
+    mat73.loadmat('./train/train/digitStruct.mat', use_attrdict=True)
 
 for j in range(1, num+1):
     des = "../Street-View-House-Numbers-Detection/data/svhn/train/"
@@ -36,21 +36,21 @@ for j in range(1, num+1):
                 label = 0
             else:
                 label = int(data['label'][i])
-            height, left, top, width =
-            data['height'][i], data['left'][i],
+            height, left, top, width =\
+                data['height'][i], data['left'][i],
             data['top'][i], data['width'][i]
-            center_x, center_y, box_w, box_h =
-            (left+width/2)/w, (top+height/2)/h, width/w, height/h
-            s = "{} {} {} {} {}\n"
-            .format(label, center_x, center_y, box_w, box_h)
+            center_x, center_y, box_w, box_h = \
+                (left+width/2)/w, (top+height/2)/h, width/w, height/h
+            s = "{} {} {} {} {}\n" \
+                .format(label, center_x, center_y, box_w, box_h)
             file.write(s)
     else:
         if data['label'] == 10:
             label = 0
         else:
             label = int(data['label'])
-        height, left, top, width =
-        data['height'], data['left'], data['top'], data['width']
+        height, left, top, width = \
+            data['height'], data['left'], data['top'], data['width']
         s = "{} {} {} {} {}\n".format(label, center_x, center_y, box_w, box_h)
         file.write(s)
     file.close()
