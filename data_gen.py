a0972960a0832b92a0f1fff5f462a5a802fc1ee0
RodyCoco <55498247+RodyCoco@users.noreply.github.com>
2021-11-25 20:04:21 +0800

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -6,6 +6,7 @@ import torchvision.transforms as tfs
 
 GPU_NUMBER = 0
 
+
 class DigitDataset(data.Dataset):
     def __init__(self, data, main_dir, transform):
         super(DigitDataset, self).__init__()
@@ -21,13 +22,12 @@ class DigitDataset(data.Dataset):
         image = Image.open(img_loc).convert("RGB")
         image = self.transform(image)
         return image, self.data[idx]-1
-    
+
+
 def get_all_image_size(main_dir, num):
     image_size_list = []
-    for i in range(1,num+1):
+    for i in range(1, num+1):
         img_loc = os.path.join(main_dir, f"{i}.png")
         image = Image.open(img_loc).convert("RGB")
-        # image = image.crop((77, 29, 77+23, 29+32))  # (left, upper, right, lower)
         image_size_list.append(image.size)
     return image_size_list
-
